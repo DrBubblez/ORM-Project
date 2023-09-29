3e0a26c511f1ccdaf5b787d2604e0359a24d9675
Harry <harrisonminer59@gmail.com>
2023-09-29 18:28:45 -0400

    Product Model

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,38 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id:{
+      dataType: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name:{
+      dataType: DataTypes.STRING,
+      allowNull: false,
+    },
+    price:{
+      dataType: DataTypes.DECIMAL,
+      allowNull: false,
+      validate:{
+        isDecimal: true,
+      }
+    },
+    stock:{
+      dataType: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate:{
+        isNumeric: true,
+      }
+    },
+    category_id:{
+      dataType: DataTypes.INTEGER,
+      references:{
+        model: 'category',
+        key: 'id',
+      }
+    }
   },
   {
     sequelize,
