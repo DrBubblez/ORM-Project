3a4301b3552ed721241aa35745a061fc7072527a
Harry <harrisonminer59@gmail.com>
2023-09-29 23:31:33 -0400

    Tag Model

--- models/Tag.js
+++ models/Tag.js
@@ -6,7 +6,15 @@ class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    id:{
+      dataType: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_name:{
+      dataType: DataTypes.STRING,
+    }
   },
   {
     sequelize,
